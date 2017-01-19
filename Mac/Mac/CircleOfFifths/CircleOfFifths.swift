5f0c737a67710bcbaf2259a80a83cd9161ba7617
cemolcay <ccemolcay@gmail.com>
2017-01-19 19:23:53 +0300

    Flip angles for mac coord system

--- Mac/Mac/CircleOfFifths/CircleOfFifths.swift
+++ Mac/Mac/CircleOfFifths/CircleOfFifths.swift
@@ -45,34 +45,34 @@ internal extension NoteType {
   internal var circleStartAngle: CGFloat {
     switch self {
     case .c: return -15
-    case .g: return 15
-    case .d: return 45
-    case .a: return 75
-    case .e: return 105
-    case .b: return 135
+    case .f: return 15
+    case .bFlat: return 45
+    case .eFlat: return 75
+    case .aFlat: return 105
+    case .dFlat: return 135
     case .gFlat: return 165
-    case .dFlat: return 195
-    case .aFlat: return 225
-    case .eFlat: return 255
-    case .bFlat: return 285
-    case .f: return 315
+    case .b: return 195
+    case .e: return 225
+    case .a: return 255
+    case .d: return 285
+    case .g: return 315
     }
   }
 
   internal var circleEndAngle: CGFloat {
     switch self {
     case .c: return 15
-    case .g: return 45
-    case .d: return 75
-    case .a: return 105
-    case .e: return 135
-    case .b: return 165
+    case .f: return 45
+    case .bFlat: return 75
+    case .eFlat: return 105
+    case .aFlat: return 135
+    case .dFlat: return 165
     case .gFlat: return 195
-    case .dFlat: return 225
-    case .aFlat: return 255
-    case .eFlat: return 285
-    case .bFlat: return 315
-    case .f: return 345
+    case .b: return 225
+    case .e: return 255
+    case .a: return 285
+    case .d: return 315
+    case .g: return 345
     }
   }
 }
