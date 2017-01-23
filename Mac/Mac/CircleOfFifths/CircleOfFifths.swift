2c91e50372eb4d5a60d1e6c257ddeab18ca950da
cemolcay <ccemolcay@gmail.com>
2017-01-23 04:51:30 +0300

    Add intervals of chords

--- Mac/Mac/CircleOfFifths/CircleOfFifths.swift
+++ Mac/Mac/CircleOfFifths/CircleOfFifths.swift
@@ -105,21 +105,25 @@ public class CircleOfFifths: UIView {
   @IBInspectable public var majorColor: UIColor = .red { didSet { draw() }}
   @IBInspectable public var minorColor: UIColor = .blue { didSet { draw() }}
   @IBInspectable public var diminishedColor: UIColor = .green { didSet { draw() }}
-  @IBInspectable public var majorTextColor: UIColor = .white { didSet { draw() }}
-  @IBInspectable public var minorTextColor: UIColor = .white { didSet { draw() }}
-  @IBInspectable public var diminishedTextColor: UIColor = .white { didSet { draw() }}
+  @IBInspectable public var majorTextColor: UIColor = .black { didSet { draw() }}
+  @IBInspectable public var minorTextColor: UIColor = .black { didSet { draw() }}
+  @IBInspectable public var diminishedTextColor: UIColor = .black { didSet { draw() }}
   @IBInspectable public var majorFontSize: CGFloat = 15 { didSet { draw() }}
   @IBInspectable public var minorFontSize: CGFloat = 15 { didSet { draw() }}
   @IBInspectable public var diminishedFontSize: CGFloat = 15 { didSet { draw() }}
   @IBInspectable public var majorTextTreshold: CGFloat = 5 { didSet { draw() }}
   @IBInspectable public var minorTextTreshold: CGFloat = 5 { didSet { draw() }}
   @IBInspectable public var diminishedTextTreshold: CGFloat = 5 { didSet { draw() }}
+  @IBInspectable public var intervalPieHeight: CGFloat = 10 { didSet { draw() }}
+  @IBInspectable public var intervalFontSize: CGFloat = 15 { didSet { draw() }}
+  @IBInspectable public var intervalTextTreshold: CGFloat = 15 { didSet { draw() }}
 
   private var circle: [NoteType] = [.c, .g, .d, .a, .e, .b, .gFlat, .dFlat, .aFlat, .eFlat, .bFlat, .f]
   private var chords: [CircleChordType] = [.major, .major, .major, .minor, .minor, .minor, .diminished]
 
   private var chordPie: PieChartLayer?
   private var circlePie: PieChartLayer?
+  private var intervalPie: PieChartLayer?
   private var selectedSlice: PieChartSlice?
   private var majorArcText: ArcTextLayer?
   private var minorArcText: ArcTextLayer?
@@ -184,12 +188,23 @@ public class CircleOfFifths: UIView {
       }))
 
 
+    intervalPie = PieChartLayer(
+      radius: 0,
+      center: center,
+      slices: chords.map({ _ in
+        PieChartSlice(
+          startAngle: 0,
+          endAngle: 0,
+          color: defaultColor)
+      }))
+
     majorArcText = ArcTextLayer()
     minorArcText = ArcTextLayer()
     dimArcText = ArcTextLayer()
 
     guard let chordPie = chordPie,
       let circlePie = circlePie,
+      let intervalPie = intervalPie,
       let majorArcText = majorArcText,
       let minorArcText = minorArcText,
       let dimArcText = dimArcText
@@ -198,6 +213,7 @@ public class CircleOfFifths: UIView {
     wantsLayer = true
     layer?.addSublayer(chordPie)
     layer?.addSublayer(circlePie)
+    layer?.addSublayer(intervalPie)
     layer?.addSublayer(majorArcText)
     layer?.addSublayer(minorArcText)
     layer?.addSublayer(dimArcText)
@@ -205,7 +221,8 @@ public class CircleOfFifths: UIView {
 
   private func draw() {
     guard let chordPie = chordPie,
-      let circlePie = circlePie
+      let circlePie = circlePie,
+      let intervalPie = intervalPie
       else { return }
 
     // Set layer colors
@@ -213,17 +230,22 @@ public class CircleOfFifths: UIView {
     chordPie.lineWidth = chordPieLineWidth
     circlePie.strokeColor = circlePieLineColor.cgColor
     circlePie.lineWidth = circlePieLineWidth
+    intervalPie.strokeColor = circlePieLineColor.cgColor
+    intervalPie.lineWidth = circlePieLineWidth
 
     // Set size
     let radius = max(0, min(frame.size.width, frame.size.height) / 2)
     chordPie.radius = radius
     circlePie.radius = max(0, radius - chordPieHeight)
+    intervalPie.radius = max(0, intervalPieHeight)
 
     // Set position
     let center = CGPoint(x: bounds.midX, y: bounds.midY)
     chordPie.center = center
     circlePie.center = center
     circlePie.labelPositionTreshold = textTreshold
+    intervalPie.center = center
+    intervalPie.labelPositionTreshold = intervalTextTreshold
 
     // Determine chord notes
     let rootNote = scale.key + scale.type.circleModeRootInterval
@@ -244,6 +266,17 @@ public class CircleOfFifths: UIView {
         let chordSlice = chordPie.slices[noteIndex]
         chordSlice.startAngle = note.circleStartAngle
         chordSlice.endAngle = note.circleEndAngle
+
+        let intervalSlice = intervalPie.slices[noteIndex]
+        intervalSlice.startAngle = note.circleStartAngle
+        intervalSlice.endAngle = note.circleEndAngle
+        intervalSlice.attributedString = NSAttributedString(
+          string: toRomanInterval(note: note, chordMode: chords[noteIndex]),
+          attributes: [
+            NSForegroundColorAttributeName: textColor,
+            NSFontAttributeName: UIFont.systemFont(ofSize: intervalFontSize)
+          ])
+
         noteSlice.isEnabled = true
       } else {
         noteSlice.isEnabled = false
@@ -251,8 +284,8 @@ public class CircleOfFifths: UIView {
     }
 
     // Draw chord type over chord pie
-    let majorAngle = chordNotes[4].circleStartAngle + 15 + 90
-    let minorAngle = chordNotes[1].circleStartAngle + 15 + 90
+    let majorAngle = chordNotes[1].circleStartAngle + 15 + 90
+    let minorAngle = chordNotes[4].circleStartAngle + 15 + 90
     let dimAngle = chordNotes[6].circleStartAngle + 15 + 90
 
     majorArcText?.text = NSAttributedString(
@@ -284,5 +317,28 @@ public class CircleOfFifths: UIView {
     dimArcText?.angle = dimAngle
     dimArcText?.radius = radius - diminishedTextTreshold
     dimArcText?.frame = bounds
-  }  
+  }
+
+  private func toRomanInterval(note: NoteType, chordMode: CircleChordType) -> String {
+    let notes = scale.noteTypes
+    let index = Int(notes.index(of: note) ?? 10)
+    var roman = ""
+    switch index {
+    case 0: roman = "i"
+    case 1: roman = "ii"
+    case 2: roman = "iii"
+    case 3: roman = "iv"
+    case 4: roman = "v"
+    case 5: roman = "vi"
+    case 6: roman = "vii"
+    case 7: roman = "viii"
+    default: roman = ""
+    }
+
+    switch chordMode {
+    case .major: return roman.uppercased()
+    case .diminished: return "\(roman)°"
+    default: return roman
+    }
+  }
 }
